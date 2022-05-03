85f016ac5b658fa7e03a156f48a343bae276100c
Toby O'Sullivan <toby.osullivan@citrix.com>
2022-05-03 12:41:38 +0100

    Function clauses

--- little-elixir/length_converter.ex
+++ little-elixir/length_converter.ex
@@ -1,13 +1,5 @@
 defmodule MeterToLengthConverter do
-  defmodule Feet do
-    def convert(m) do
-      m * 3.28084
-    end
-  end
-
-  defmodule Inch do
-    def convert(m) do
-      m * 39.3701
-    end
-  end
+    def convert(:feet, m), do: m * 3.28084
+    def convert(:inch, m), do: m * 39.3701
+    def convert(:yard, m), do: m * 1.09361
 end
